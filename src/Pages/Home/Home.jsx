c9a463a4026d1cb3d24685e6ee331cac59edd16c
Shakib AL Nur <shakibalnur3268@gmail.com>
2023-08-10 12:26:57 +0600

    new section

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import ChooseWork from './ChooseWork/ChooseWork';
 
 const Home = () => {
     return (
         <div>
             home
+            <ChooseWork></ChooseWork>
         </div>
     );
 };
