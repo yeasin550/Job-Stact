c77dc6e98e994754bd60c1be549d9ce2aff9639d
Ashikur Rahman <ashikmoinkhan@gmail.com>
2023-08-10 16:04:03 +0600

    Added Banner Section

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import ChooseWork from './ChooseWork/ChooseWork';
 import Contact from './HomeChildren/Contact/Contact';
+import BrowseJob from './HomeChildren/BrowseJobs/BrowseJob';
+import Banner from './HomeChildren/Banner/Banner';
 
 const Home = () => {
     return (
         <div>
-            home
+            <Banner></Banner>
+           <BrowseJob></BrowseJob>
             <ChooseWork></ChooseWork>
             <Contact></Contact>
         </div>
